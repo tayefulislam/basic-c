86423a4c401b4623212414cbb5f31ebcf82bcb99
tayefulislam <kazitayefulislam12@gmail.com>
2022-08-07 12:26:16 +0600

    if else to check multi conditon and get multi result

--- ifelse.c
+++ ifelse.c
@@ -8,22 +8,37 @@ int main()
 
     // if else to get singel result
 
-    if (age >= 18)
+    /*
+        if (age >= 18)
+        {
+            printf("Will get NID Card\n");
+        }
+
+        else if (age >= 13 && age < 18)
+        {
+            printf("can get birth certificate\n");
+        }
+
+        else
+        {
+            printf("Plase wait for 2 years\n");
+        }
+
+        printf("Thank you for give the information\n");
+
+    */
+
+    // if else to check multi conditon and get multi result
+
+    if (age >= 20)
     {
-        printf("Will get NID Card\n");
+        printf("get go abroad\n");
     }
 
-    else if (age >= 13 && age < 18)
+    if (age >= 18 && age <= 21)
     {
-        printf("can get birth certificate\n");
+        printf("Do anything\n");
     }
-
-    else
-    {
-        printf("Plase wait for 2 years\n");
-    }
-
-    printf("Thank you for give the information\n");
 
     return 0;
 }
