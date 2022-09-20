ca38542766f6a505f8316a58b64993ceda4198b5
tayefulislam <kazitayefulislam12@gmail.com>
2022-09-20 23:33:23 +0600

    function and recursion chapter start

---
+++ function&recursion/function.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void printHello(); // functon call
+
+int main()
+{
+    printHello(); // function call
+    return 0;
+}
+
+// funtion defination
+void printHello()
+{
+    printf("Hello\n");
+}
